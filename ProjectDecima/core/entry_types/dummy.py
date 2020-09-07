b08a30e33ed1462025e772bee4da88bffa7279fe
REDxEYE <med45c@gmail.com>
2020-09-07 11:07:32 +0300

    add dump function and exportable flag to CoreDummy

--- ProjectDecima/core/entry_types/dummy.py
+++ ProjectDecima/core/entry_types/dummy.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from uuid import UUID
 
 from ...utils.byte_io_ds import ByteIODS
@@ -5,6 +6,8 @@ from ...core.pod.core_header import CoreHeader
 
 
 class CoreDummy:
+    exportable = False
+
     def __init__(self, ):
         self.header = CoreHeader()
         self.guid = UUID(int=0)
@@ -13,6 +16,9 @@ class CoreDummy:
         self.header.parse(reader)
         self.guid = reader.read_guid()
         reader.skip(self.header.size - 16)
+
+    def dump(self, output_path: Path):
+        pass
 
     def __repr__(self):
         return f'<{self.__class__.__name__} {self.guid}>'
