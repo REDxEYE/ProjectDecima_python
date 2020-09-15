942b8ca3f747f2e4bb22fbf3e70150d945d1fe81
REDxEYE <med45c@gmail.com>
2020-09-15 21:09:44 +0300

    optimize chunk decryption even more

--- ProjectDecima/utils/decryption.py
+++ ProjectDecima/utils/decryption.py
@@ -12,22 +12,18 @@ except ImportError:
 
 
 def decrypt(input_key: List[bytes], data: np.ndarray):
-    return np.bitwise_xor(data, np.frombuffer(mmh3.hash_bytes(input_key[0], seed) +
-                                              mmh3.hash_bytes(input_key[1], seed), np.uint32, 8))
+    return np.bitwise_xor(data, np.frombuffer(mmh3.hash_bytes(input_key[0], seed) + mmh3.hash_bytes(input_key[1], seed),
+                                              np.uint32, 8))
 
 
-def decrypt_chunk_data(data, input_key):
+def decrypt_chunk_data(data: bytes, input_key: bytes):
     iv: np.ndarray = np.frombuffer(mmh3.hash_bytes(input_key, seed), np.uint32, 4)
     iv = np.bitwise_xor(iv, np.array(encryption_key_2, np.uint32))
     digest = np.frombuffer(md5(iv.tobytes()).digest(), np.uint8, 16)
     size = len(data)
-    tmp = np.zeros((size + 16 - (size % 16),), dtype=np.uint8)
-    tmp[:size] = np.frombuffer(data, np.uint8)
-    # data: np.ndarray = np.frombuffer(data, np.uint8)
-    # size = data.size
-    # data = np.append(data, np.zeros((16 - (size % 16)), np.uint8))
-    data = tmp.reshape((-1, 16))
-    data = np.bitwise_xor(data, digest)[:size]
+    data = np.frombuffer(data, np.uint8)
+    data = np.resize(data, (size + 16 - (size % 16),))
+    data = np.bitwise_xor(data.reshape((-1, 16)), digest)
     return data.tobytes()[:size]
 
 
