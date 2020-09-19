2fe82051b84f66e4a5a1fd981848b81a76fc7974
REDxEYE <med45c@gmail.com>
2020-09-19 23:36:44 +0300

    add data from recently acquired .core RTTI data

--- ProjectDecima/core/entry_types/texture.py
+++ ProjectDecima/core/entry_types/texture.py
@@ -8,22 +8,91 @@ from ...core.stream_reference import StreamingDataSource
 from ...utils.byte_io_ds import ByteIODS
 
 
+class ETextureType(IntEnum):
+    Tex2D = 0
+    Tex3D = 1
+    TexCubeMap = 2
+    Tex2DArray = 3
+
+
 class TexturePixelFormat(IntEnum):
-    UNK = 0
-    RGBA8 = 0xC
-    UNK_FORMAT_0 = 0x12
-    UNK_FORMAT_1 = 0x13
-    UNK_FORMAT_2 = 0x16
-    UNK_FORMAT_3 = 0x1C
-    A8 = 0x1F
-    UNK_FORMAT_4 = 0x31
-    BC1 = 0x42
-    BC2 = 0x43
-    BC3 = 0x44
-    BC4 = 0x45
-    BC5 = 0x47
-    BC6 = 0x49
-    BC7 = 0x4B
+    INVALID = 76
+    RGBA_5551 = 0
+    RGBA_5551_REV = 1
+    RGBA_4444 = 2
+    RGBA_4444_REV = 3
+    RGB_888_32 = 4
+    RGB_888_32_REV = 5
+    RGB_888 = 6
+    RGB_888_REV = 7
+    RGB_565 = 8
+    RGB_565_REV = 9
+    RGB_555 = 10
+    RGB_555_REV = 11
+    RGBA_8888 = 12
+    RGBA_8888_REV = 13
+    RGBE_REV = 14
+    RGBA_FLOAT_32 = 15
+    RGB_FLOAT_32 = 16
+    RG_FLOAT_32 = 17
+    R_FLOAT_32 = 18
+    RGBA_FLOAT_16 = 19
+    RGB_FLOAT_16 = 20
+    RG_FLOAT_16 = 21
+    R_FLOAT_16 = 22
+    RGBA_UNORM_32 = 23
+    RG_UNORM_32 = 24
+    R_UNORM_32 = 25
+    RGBA_UNORM_16 = 26
+    RG_UNORM_16 = 27
+    R_UNORM_16 = 28
+    RGBA_UNORM_8 = 29
+    RG_UNORM_8 = 30
+    R_UNORM_8 = 31
+    RGBA_NORM_32 = 32
+    RG_NORM_32 = 33
+    R_NORM_32 = 34
+    RGBA_NORM_16 = 35
+    RG_NORM_16 = 36
+    R_NORM_16 = 37
+    RGBA_NORM_8 = 38
+    RG_NORM_8 = 39
+    R_NORM_8 = 40
+    RGBA_UINT_32 = 41
+    RG_UINT_32 = 42
+    R_UINT_32 = 43
+    RGBA_UINT_16 = 44
+    RG_UINT_16 = 45
+    R_UINT_16 = 46
+    RGBA_UINT_8 = 47
+    RG_UINT_8 = 48
+    R_UINT_8 = 49
+    RGBA_INT_32 = 50
+    RG_INT_32 = 51
+    R_INT_32 = 52
+    RGBA_INT_16 = 53
+    RG_INT_16 = 54
+    R_INT_16 = 55
+    RGBA_INT_8 = 56
+    RG_INT_8 = 57
+    R_INT_8 = 58
+    RGB_FLOAT_11_11_10 = 59
+    RGBA_UNORM_10_10_10_2 = 60
+    RGB_UNORM_11_11_10 = 61
+    DEPTH_FLOAT_32_STENCIL_8 = 62
+    DEPTH_FLOAT_32_STENCIL_0 = 63
+    DEPTH_24_STENCIL_8 = 64
+    DEPTH_16_STENCIL_0 = 65
+    BC1 = 66
+    BC2 = 67
+    BC3 = 68
+    BC4U = 69
+    BC4S = 70
+    BC5U = 71
+    BC5S = 72
+    BC6U = 73
+    BC6S = 74
+    BC7 = 75
 
 
 class Texture(CoreDummy):
@@ -31,10 +100,11 @@ class Texture(CoreDummy):
 
     def __init__(self):
         super().__init__()
-        self.unk_0 = 0
+        self.texture_type = 0
         self.width = 0
         self.height = 0
         self.layer_count = 0
+        self.tex_3d_depth = 0
         self.total_mips = 0
         self.pixel_format = TexturePixelFormat(0)
         self.unk_1 = 0
@@ -52,7 +122,15 @@ class Texture(CoreDummy):
     def parse(self, reader: ByteIODS, core_file):
         self.header.parse(reader)
         self.guid = reader.read_guid()
-        self.unk_0, self.width, self.height, self.layer_count = reader.read_fmt('4H')
+        self.texture_type = ETextureType(reader.read_uint8())
+        reader.skip(1)
+        self.width, self.height = reader.read_fmt('2H')
+        if self.texture_type in [ETextureType.Tex2D, ETextureType.TexCubeMap]:
+            reader.read_uint16()
+        elif self.texture_type == ETextureType.Tex3D:
+            self.tex_3d_depth = 1 << reader.read_uint16()
+        elif self.texture_type == ETextureType.Tex2DArray:
+            self.layer_count = reader.read_uint16()
         self.total_mips = reader.read_uint8()
         self.pixel_format = TexturePixelFormat(reader.read_uint8())
         self.unk_1 = reader.read_uint16()
@@ -71,13 +149,13 @@ class Texture(CoreDummy):
         except ImportError:
             return
         pixel_type = {
-            TexturePixelFormat.RGBA8: ('RGBA', 'raw', 4),
-            TexturePixelFormat.A8: ('L', 'raw', 1),
+            TexturePixelFormat.RGBA_8888: ('RGBA', 'raw', 4),
+            TexturePixelFormat.R_UNORM_8: ('L', 'raw', 1),
             TexturePixelFormat.BC1: ('RGBA', ('bcn', 1, 0), 0.5),
             TexturePixelFormat.BC2: ('RGBA', ('bcn', 2, 0), 1),
             TexturePixelFormat.BC3: ('RGBA', ('bcn', 3, 0), 1),
-            TexturePixelFormat.BC4: ('L', ('bcn', 4, 0), 0.5),
-            TexturePixelFormat.BC5: ('RGBA', ('bcn', 5, 0), 1),
+            TexturePixelFormat.BC4U: ('L', ('bcn', 4, 0), 0.5),
+            TexturePixelFormat.BC5U: ('RGBA', ('bcn', 5, 0), 1),
             TexturePixelFormat.BC7: ('RGBA', ('bcn', 7, 0), 1),
         }
         if self.stream:
--- ProjectDecima/core/entry_types/texture_set.py
+++ ProjectDecima/core/entry_types/texture_set.py
@@ -1,3 +1,4 @@
+from enum import IntEnum
 from typing import List
 
 from . import CoreDummy
@@ -7,44 +8,111 @@ from ...utils.byte_io_ds import ByteIODS
 from ..entry_reference import EntryReference
 
 
-class TSEntry:
+class EImageCompressionMethod(IntEnum):
+    PerceptualData = 0
+    NormalData = 1
+    VariableData = 2
+    DefaultData = 3
+
+
+class ETexColorSpace(IntEnum):
+    Linear = 0
+    sRGB = 1
+
+
+class ETextureSetType(IntEnum):
+    Invalid = 0
+    Color = 1
+    Alpha = 2
+    Normal = 3
+    Reflectance = 4
+    AO = 5
+    Roughness = 6
+    Height = 7
+    Mask = 8
+    Mask_Alpha = 9
+    Incandescence = 10
+    Translucency_Diffusion = 11
+    Translucency_Amount = 12
+    Misc_01 = 13
+    Count = 14
+
+
+class ETextureSetStorageType(IntEnum):
+    RGB = 0
+    R = 1
+    G = 2
+    B = 3
+    A = 4
+    Count = 5
+
+
+class ETextureSetQualityType(IntEnum):
+    Default = 0
+    Compressed_High = 1
+    Compressed_Low = 2
+    Uncompressed = 3
+    Normal_BC6 = 4
+    Normal_High = 5
+    Normal_Low = 6
+    BC4 = 8
+    Clean = 7
+    NormalRoughnessBC7 = 9
+    AlphaToCoverageBC4 = 10
+    Count = 11
+
+
+class ETexAddress(IntEnum):
+    Wrap = 0
+    Clamp = 1
+    Mirror = 2
+    ClampToBorder = 3
+
+
+class TextureSetEntry:
 
     def __init__(self):
-        self.unk_0 = 0
-        self.unk_1 = 0
-        self.unk_2 = 0
-        self.unk_3 = 0
-        self.unk_4 = 0
-        self.ref = EntryReference()
+        self.compression_method = EImageCompressionMethod.PerceptualData
+        self.create_mipmaps = 0
+        self.color_space = ETexColorSpace.Linear
+        self.packing_info = 0
+        self.texture_type = 0
+        self.texture_ref = EntryReference()
 
     def parse(self, reader: ByteIODS, core_file):
-        self.unk_0, self.unk_1, self.unk_2, self.unk_3, self.unk_4 = reader.read_fmt('IIIIB')
-        self.ref.parse(reader, core_file)
+        self.compression_method = EImageCompressionMethod(reader.read_uint32())
+        self.create_mipmaps = reader.read_uint8()
+        self.color_space = ETexColorSpace(reader.read_uint32())
+        self.packing_info = reader.read_uint32()
+        self.texture_type = reader.read_uint32()
+        self.texture_ref.parse(reader, core_file)
 
 
-class SrcEntry:
+class TextureSetTextureDesc:
     def __init__(self):
-        self.slot_id = 0
-        self.src_name = HashedString()
-        self.unk_0 = 0
-        self.unk_1 = 0
-        self.unk_2 = 0
-        self.unk_3 = 0
-        # self.unk_pad = 0
+        self.texture_type = ETextureSetType.Invalid
+        self.path = HashedString()
+        self.active = 0
+        self.gamma_space = 0
+        self.storage_type = ETextureSetStorageType.RGB
+        self.quality_type = ETextureSetQualityType.Default
+        self.compression_method = EImageCompressionMethod.PerceptualData
         self.width = 0
         self.height = 0
-        self.unks_5 = []
+        self.default_color = []
 
     def parse(self, reader: ByteIODS):
-        self.slot_id = reader.read_uint32()
-        self.src_name = reader.read_hashed_string()
-        self.unk_0 = reader.read_uint16()
-        self.unk_1, self.unk_2, self.unk_3 = reader.read_fmt('3I')
-        if self.unk_0 == 0:
-            self.unk_pad = reader.read_uint32()
+        self.texture_type = ETextureSetType(reader.read_uint32())
+        self.path = reader.read_hashed_string()
+        self.active, self.gamma_space = reader.read_fmt('2B')
+        self.storage_type = ETextureSetStorageType(reader.read_uint32())
+        self.quality_type = ETextureSetQualityType(reader.read_uint32())
+        self.compression_method = EImageCompressionMethod(reader.read_uint32())
+        if self.active == 0:
+            reader.skip(4)
         else:
             self.width, self.height = reader.read_fmt('2I')
-        self.unks_5 = reader.read_fmt('4f')
+        self.default_color = reader.read_fmt('4f')
 
 
 class TextureSet(CoreDummy):
@@ -52,24 +120,26 @@ class TextureSet(CoreDummy):
 
     def __init__(self):
         super().__init__()
-        self.ts_entries: List[TSEntry] = []
-        self.unk_0 = 0
-        self.src_entries: List[SrcEntry] = []
+        self.entries: List[TextureSetEntry] = []
+        self.mipmap_mode = ETexAddress.Wrap
+        self.descriptions: List[TextureSetTextureDesc] = []
+        self.preset = EntryReference()
 
     def parse(self, reader: ByteIODS, core_file):
         self.header.parse(reader)
         self.guid = reader.read_guid()
         entry_count = reader.read_uint32()
         for _ in range(entry_count):
-            entry = TSEntry()
+            entry = TextureSetEntry()
             entry.parse(reader, core_file)
-            self.ts_entries.append(entry)
-        self.unk_0 = reader.read_uint32()
+            self.entries.append(entry)
+        self.mipmap_mode = ETexAddress(reader.read_uint32())
         src_entry_count = reader.read_uint32()
         for _ in range(src_entry_count):
-            entry = SrcEntry()
+            entry = TextureSetTextureDesc()
             entry.parse(reader)
-            self.src_entries.append(entry)
+            self.descriptions.append(entry)
+        self.preset.parse(reader, core_file)
 
 
 EntryTypeManager.register_handler(TextureSet)
