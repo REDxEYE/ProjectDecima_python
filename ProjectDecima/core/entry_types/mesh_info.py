900498013269a7982ef7b48bcec62f00ca99fc93
REDxEYE <med45c@gmail.com>
2020-09-19 23:36:06 +0300

    add data from recently acquired .core RTTI data

--- ProjectDecima/core/entry_types/mesh_info.py
+++ ProjectDecima/core/entry_types/mesh_info.py
@@ -1,3 +1,4 @@
+from enum import IntEnum
 from typing import List
 from uuid import UUID
 
@@ -8,24 +9,35 @@ from ...utils.byte_io_ds import ByteIODS
 from ..entry_reference import EntryReference
 
 
-class IndicesInfo(CoreDummy):
+class EIndexFormat(IntEnum):
+    Index16 = 0
+    Index32 = 1
+
+
+class IndexArrayResource(CoreDummy):
     magic = 0x5FE633B37CEDBF84
 
     def __init__(self):
         super().__init__()
         self.indices_count = 0
-        self.unks_0 = []
-        self.unk_guid = UUID(int=0)
+        self.flags = 0
+        self.format = EIndexFormat.Index16
+        self.is_streaming = 0
+        self.resource_guid = UUID(int=0)
 
     def parse(self, reader: ByteIODS, core_file):
         self.header.parse(reader)
         self.guid = reader.read_guid()
         self.indices_count = reader.read_uint32()
-        self.unks_0 = reader.read_fmt('3I')
-        self.unk_guid = reader.read_guid()
+        if self.indices_count > 0:
+            self.flags = reader.read_uint32()
+            self.format = EIndexFormat(reader.read_uint32())
+            self.is_streaming = reader.read_uint32()
+            assert self.is_streaming in [0, 1]
+            self.resource_guid = reader.read_guid()
 
 
-EntryTypeManager.register_handler(IndicesInfo)
+EntryTypeManager.register_handler(IndexArrayResource)
 
 
 class UnkVertexInfo(CoreDummy):
@@ -70,33 +82,41 @@ class MeshStreamInfo(CoreDummy):
 EntryTypeManager.register_handler(MeshStreamInfo)
 
 
-class MeshInfo(CoreDummy):
+class PrimitiveResource(CoreDummy):
     magic = 0xEE49D93DA4C1F4B8
 
     def __init__(self):
         super().__init__()
         self.unk_0 = 0
-        self.vertex_info = EntryReference()
-        self.indices_info = EntryReference()
+        self.vertex_array = EntryReference()
+        self.index_array = EntryReference()
+        self.bbox = ((0.0, 0.0, 0.0,), (0.0, 0.0, 0.0))
+        self.index_offset = 0
+        self.skd_tree = EntryReference()
         self.index_count = 0
+        self.index_end = 0
+        self.hash = 0
 
     def parse(self, reader: ByteIODS, core_file):
         self.header.parse(reader)
         self.guid = reader.read_guid()
         self.unk_0 = reader.read_uint32()
-        self.vertex_info.parse(reader, core_file)
-        self.indices_info.parse(reader, core_file)
-        reader.skip(24)
-        reader.skip(5)
+        self.vertex_array.parse(reader, core_file)
+        self.index_array.parse(reader, core_file)
+        self.index_offset = reader.read_uint32()
+        self.bbox = (reader.read_fmt('3f'), reader.read_fmt('3f'))
+        self.skd_tree.parse(reader, core_file)
         self.index_count = reader.read_uint32()
+        self.index_end = reader.read_uint32()
+        # self.hash = reader.read_uint32()
 
 
-EntryTypeManager.register_handler(MeshInfo)
+EntryTypeManager.register_handler(PrimitiveResource)
 
 
 class VertexBlockInfo:
     def __init__(self):
-        self.unk_0 = 0
+        self.flags = 0
         self.stride = 0
         self.element_count = 0
         self.unk_3 = 0
@@ -104,32 +124,32 @@ class VertexBlockInfo:
         self.unk_data = []
 
     def parse(self, reader: ByteIODS):
-        self.unk_0, self.stride, self.element_count, self.unk_3 = reader.read_fmt('4I')
+        self.flags, self.stride, self.element_count, self.unk_3 = reader.read_fmt('4I')
         self.unk_4 = reader.read_fmt('3I')
         for _ in range(self.element_count):
-            self.unk_data.append(reader.read_uint32())
+            self.unk_data.append(reader.read_bytes(4))
 
 
-class Vertices(CoreDummy):
+class VertexArrayResource(CoreDummy):
     magic = 0x3AC29A123FAABAB4
 
     def __init__(self):
         super().__init__()
         self.vertex_count = 0
-        self.block_count = 0
-        self.unk_1 = 1
+        self.vertex_stream_count = 0
+        self.is_streaming = 0
         self.vertex_infos: List[VertexBlockInfo] = []
 
     def parse(self, reader: ByteIODS, core_file):
         self.header.parse(reader)
         self.guid = reader.read_guid()
         self.vertex_count = reader.read_uint32()
-        self.block_count = reader.read_uint32()
-        self.unk_1 = reader.read_uint8()
-        for _ in range(self.block_count):
+        self.vertex_stream_count = reader.read_uint32()
+        self.is_streaming = reader.read_uint8()
+        for _ in range(self.vertex_stream_count):
             vertex_block_info = VertexBlockInfo()
             vertex_block_info.parse(reader)
             self.vertex_infos.append(vertex_block_info)
 
 
-EntryTypeManager.register_handler(Vertices)
+EntryTypeManager.register_handler(VertexArrayResource)
